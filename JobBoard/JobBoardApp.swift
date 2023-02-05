db9decc0a23687f2f52001aa8854b218d73ba17e
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2023-02-05 20:33:31 +0300

    Initial Commit

---
+++ JobBoard/JobBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobBoardApp.swift
+//  JobBoard
+//
+//  Created by Ian Nalyanya on 05/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct JobBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
